2d9ba4216d75caf9ceed43f927bd6dfdefdf140b
Jaime Alonso <jaimea111@gmail.com>
2023-08-03 18:21:43 -0500

    Adding dataseet to the database!

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -1,4 +1,5 @@
-﻿using CityInfo.API.Entities;
+﻿using CityInfo.API.Controllers;
+using CityInfo.API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.DbContexts;
@@ -11,7 +12,66 @@ public class CityInfoContext : DbContext
     public CityInfoContext(DbContextOptions<CityInfoContext> options) 
         : base(options)
     {
-        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>().HasData(
+            new City("New York City")
+            {
+                Id = 1,
+                Description = "The one with that big park."
+            },
+            new City("Antwerp")
+            {
+                Id = 2,
+                Description = "The one with the cathedral that was never finished."
+            },
+            new City("Paris")
+            {
+                Id = 3,
+                Description = "The one with that big tower."
+            });
+
+        modelBuilder.Entity<PointOfInterest>()
+            .HasData(
+                new PointOfInterest("Central Park A")
+                {
+                    Id=1,
+                    CityId = 1,
+                    Description = "The most visited urban park in the United States."
+                },
+                new PointOfInterest("Central Park B")
+                {
+                    Id=2,
+                    CityId = 1,
+                    Description = "A 102-story skyscraper located in Midtown Manhattan"
+                },
+                new PointOfInterest("Cathedral")
+                {
+                    Id=3,
+                    CityId = 2,
+                    Description = "A gothic style cathedral, conceived by architects"
+                },
+                new PointOfInterest("Cochabamba")
+                {
+                    Id=4,
+                    CityId = 2,
+                    Description = "A popular street."
+                },
+                new PointOfInterest("Statue A")
+                {
+                    Id=5,
+                    CityId = 3,
+                    Description = "A random statue."
+                },
+                new PointOfInterest("Statue Maccarron")
+                {
+                    Id=6,
+                    CityId = 3,
+                    Description = "A maccarron statue."
+                });
+        base.OnModelCreating(modelBuilder);
     }
 
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ CityInfo.API/Migrations/20230803232102_DataSeed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The one with that big park.", "New York City" },
+                    { 2, "The one with the cathedral that was never finished.", "Antwerp" },
+                    { 3, "The one with that big tower.", "Paris" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The most visited urban park in the United States.", "Central Park A" },
+                    { 2, 1, "A 102-story skyscraper located in Midtown Manhattan", "Central Park B" },
+                    { 3, 2, "A gothic style cathedral, conceived by architects", "Cathedral" },
+                    { 4, 2, "A popular street.", "Cochabamba" },
+                    { 5, 3, "A random statue.", "Statue A" },
+                    { 6, 3, "A maccarron statue.", "Statue Maccarron" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
